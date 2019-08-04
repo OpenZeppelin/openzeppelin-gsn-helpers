b5ad89dd8c72d0030c6fd62e3a25e09139bd69f4
Santiago Palladino <spalladino@gmail.com>
2019-08-04 11:41:56 -0300

    Export relayhub data

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const { registerRelay, deployRelayHub, fundRecipient} = require('./src/helpers');
+const { registerRelay, deployRelayHub, fundRecipient } = require('./src/helpers');
+const { relayHub } = require('./src/data');
 const expectError = require('./src/expectError');
+const _ = require('lodash');
 
 module.exports = {
   registerRelay,
   deployRelayHub,
   fundRecipient,
   expectError,
+  relayHub: _.pick(relayHub, ['abi', 'address', 'bytecode'])
 };
