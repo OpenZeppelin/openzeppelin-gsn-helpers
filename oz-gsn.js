400077a94996ce9e614d4847ec4d3d7458260952
Santiago Palladino <spalladino@gmail.com>
2019-08-12 19:32:01 +0200

    Revert "Fail on unknown command"
    This reverts commit 1309878dcd9a596e74c08a0a257678392ebf1812.

--- oz-gsn.js
+++ oz-gsn.js
@@ -8,8 +8,4 @@ program
   .command('deploy-relay-hub', 'deploy the singleton relay hub instance')
   .command('register-relayer', 'stake for a relayer and fund it')
   .command('fund-recipient', 'fund a recipient contract so that it can receive relayed calls')
-  .on('command:*', function () {
-    console.error('Invalid command: %s\nSee --help for a list of available commands.', program.args.join(' '));
-    process.exit(1);
-  })
   .parse(process.argv);
