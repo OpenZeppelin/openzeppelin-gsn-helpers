116095bb5ba2e1ec6af74fa68dbcb9c5fa1e7a8c
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-08-29 17:01:45 -0300

    Add binary integrity check

--- src/download.js
+++ src/download.js
@@ -1,37 +1,40 @@
 const process = require('process');
 const envPaths = require('env-paths');
-const { createWriteStream, chmodSync } = require('fs');
+const { createWriteStream, chmodSync, readFileSync, unlinkSync } = require('fs');
 const { pathExists, ensureDir } = require('fs-extra');
 const { dirname } = require('path');
 const axios = require('axios');
+const { sha3 } = require('web3-utils');
 
 const REPOSITORY = 'OpenZeppelin/openzeppelin-gsn-helpers';
 const VERSION = 'v0.1.4';
 const BINARY = 'gsn-relay';
-const PATH = `${envPaths('gsn').cache}/${BINARY}-${VERSION}`;
+const BINARY_PATH = `${envPaths('gsn').cache}/${BINARY}-${VERSION}`;
+const CHECKSUMS_PATH = `${envPaths('gsn').cache}/checksums.json`;
 
-async function ensureRelayer(path=PATH) {
+async function ensureRelayer(path=BINARY_PATH) {
   if (await hasRelayer(path)) return path;
   await downloadRelayer(path);
   return path;
 }
 
-async function hasRelayer(path=PATH) {
-  return await pathExists(path);
+async function hasRelayer(path=BINARY_PATH) {
+  return await pathExists(path) && await binaryUncorrupted(path);
 }
 
-async function downloadRelayer(path=PATH) {
+async function downloadRelayer(path=BINARY_PATH) {
   await ensureDir(dirname(path));
-  const url = getUrl();
-  const writer = createWriteStream(path);
-  console.error(`Downloading relayer from ${url}`);
-  const response = await axios.get(url, { responseType: 'stream' });
-  response.data.pipe(writer);
 
-  await new Promise((resolve, reject) => {
-    writer.on('finish', resolve);
-    writer.on('error', reject);
-  });
+  const url = getUrl();
+  console.error(`Downloading relayer from ${url}`);
+  await downloadFile(url, path);
+
+  if (!await binaryUncorrupted(path)) {
+    // Remove the corrupt binary and checksums file - the issue could be in either of them
+    unlinkSync(path);
+    unlinkSync(CHECKSUMS_PATH);
+    throw Error(`Relayer binary integrity check failed, are there network issues?`);
+  }
 
   console.error(`Relayer downloaded to ${path}`);
   chmodSync(path, '775');
@@ -60,6 +63,26 @@ function getArch() {
 
 function getPath() {
   return PATH;
+}
+
+async function downloadFile(url, path) {
+  const writer = createWriteStream(path);
+  const response = await axios.get(url, { responseType: 'stream' });
+  response.data.pipe(writer);
+
+  await new Promise((resolve, reject) => {
+    writer.on('finish', resolve);
+    writer.on('error', reject);
+  });
+}
+
+async function binaryUncorrupted(path) {
+  if (!await pathExists(CHECKSUMS_PATH)) {
+    await downloadFile(`https://github.com/${REPOSITORY}/releases/download/checksums.json`, CHECKSUMS_PATH);
+  }
+
+  const checksums = JSON.parse(readFileSync(CHECKSUMS_PATH));
+  return (checksums[getPlatform()][getArch()] === sha3(readFileSync(path)));
 }
 
 module.exports = {
