8ddef77975ac6f192a91df6b014ecfd7d873d178
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-09-12 13:35:40 -0300

    Remove data dependency on balance

--- src/balance.js
+++ src/balance.js
@@ -1,17 +1,10 @@
-const data = require('./data');
 const { getRecipientAddress, getRelayHub } = require('./helpers');
 const { merge } = require('lodash');
 
 async function balance(web3, options = {}) {
-  const defaultOptions = {
-    relayHubAddress: data.relayHub.address,
-  };
-
-  options = merge(defaultOptions, options);
-
   options.recipient = getRecipientAddress(options.recipient);
 
-  const relayHub = getRelayHub(web3, options.relayHubAddress);
+  const relayHub = getRelayHub(web3);
 
   return relayHub.methods.balanceOf(options.recipient).call();
 }
